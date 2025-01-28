8955de15900e7953c95bb27b9b692784a6208a11
Fernandonov21 <fernandonovillo21@gmail.com>
2025-01-28 11:41:20 -0500

    feat: :sparkles: Add initial project structure with Flask setup and category API

---
+++ app/controllers/category_controller.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, jsonify
+from app.models.models import Category
+
+category_bp = Blueprint('category', __name__)
+
+@category_bp.route('/categories', methods=['GET'])
+def get_all_categories():
+    categories = Category.query.all()
+    if not categories:
+        return jsonify({"message": "No categories found"}), 404
+
+    categories_list = [category.to_dict() for category in categories]
+    return jsonify({"message": "Categories fetched successfully", "categories": categories_list}), 200
